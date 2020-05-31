34214c7e306f622b8b557162dc1875e8d6af8120
Rodolfo Francisco Pinto <rodolfofrancisco1990@gmail.com>
2020-05-31 14:21:40 -0300

    first commit

---
+++ src/web/NSE.WebApp.MVC/Startup.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NSE.WebApp.MVC.Configuration;
+
+namespace NSE.WebApp.MVC
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostEnvironment hostingEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(hostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddEnvironmentVariables();
+
+            if (hostingEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }       
+
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityConfiguration();
+
+            services.AddMvcConfiguration(Configuration);
+
+            services.RegisterServices();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMvcConfiguration(env);
+        }
+    }
+}
